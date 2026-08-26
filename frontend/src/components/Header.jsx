210dd5fcd9588fbb297123bd4620a38e515527b5
MOHAMMED FAIZAL <mohammedfaizal218@gmail.com>
2026-08-26 21:28:04 +0530

    feat: implement backend authentication and authorization

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header className="site-header">
+      <div className="site-header__brand">
+        DATACALIPER
+        <span>TRAINING</span>
+      </div>
+
+      <div className="site-header__status">
+        EARLY CAREER
+      </div>
+    </header>
+  )
+}
+
+export default Header
